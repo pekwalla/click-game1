8f51959766bdb66754cd6143ed71b30335cd62b7
Peggy Ekwalla <pekwalla@gmail.com>
2019-07-16 03:12:39 -0400

    Updates

---
+++ precache-manifest.6dbf51991e9c9b7901a6f8a76541c3ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e28cc3b1a45be8e53bdf",
+    "url": "/click-game/static/js/runtime~main.e28cc3b1.js"
+  },
+  {
+    "revision": "f9655a9cf3554a8cfe3e",
+    "url": "/click-game/static/js/main.f9655a9c.chunk.js"
+  },
+  {
+    "revision": "b17f661fa4d40e9da8c6",
+    "url": "/click-game/static/js/2.b17f661f.chunk.js"
+  },
+  {
+    "revision": "f9655a9cf3554a8cfe3e",
+    "url": "/click-game/static/css/main.070684eb.chunk.css"
+  },
+  {
+    "revision": "150bd1da73e1a065d3896bce992094ff",
+    "url": "/click-game/index.html"
+  }
+];
